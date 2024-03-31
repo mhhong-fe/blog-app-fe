bf86e94e6bbf635180fc3455113a9beb169752cd
mhhong <2550593431@qq.com>
2024-03-31 14:28:13 +0800

    feat: 增加默认路径

--- components.d.ts
+++ components.d.ts
@@ -7,12 +7,9 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElInput: typeof import('element-plus/es')['ElInput']
-    ElSwitch: typeof import('element-plus/es')['ElSwitch']
-    ElSwtich: typeof import('element-plus/es')['ElSwtich']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
     <div id="blog_app" class="h-100vh w-100vw">
-        <!-- <h2 class="text-4 font-500">Hello World!</h2>
-        <el-button type="primary" @click="test">开始</el-button> -->
         <router-view></router-view>
     </div>
 </template>
 
 <script setup lang="ts">
-// async function test() {
-//     const res = await request(API_HELLO_WORLD());
-//     console.log({res});
-// }
+const router = useRouter();
+if (location.pathname === '/' || location.pathname === '') {
+    router.push('/login');
+}
 </script>
 
 <style module lang="scss"></style>

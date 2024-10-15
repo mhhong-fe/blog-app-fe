b80a15d446e25e53f879c597f2020ac6975dde36
mhhong-fe <2550593431@qq.com>
2024-10-15 17:15:51 +0800

    feat: 更新首页ui

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -4,7 +4,9 @@
             <div class="font-600 text-12 lg:text-16 animate__animated animate__bounceInLeft">
                 Hello World
             </div>
-            <div>或许是不知梦的缘故，流离之人追逐幻影</div>
+            <div class="aWord">
+                或许是不知梦的缘故，流离之人追逐幻影
+            </div>
         </div>
     </div>
 </template>
@@ -14,5 +16,28 @@
 </script>
 
 <style scoped>
+.aWord {
+    position: absolute;
+    bottom: 35px;
+    right: 55px;
+    font-size: 20px;
+    font-weight: 700;
+    color: transparent;
+    background: linear-gradient(90deg, transparent 0%, rgb(216 26 26) 20%, rgb(255, 255, 255) 50%, rgb(216 26 26) 80%, transparent 100%);
+    background-size: 80%;
+    background-repeat: no-repeat;
+    -webkit-background-clip: text;
+    background-clip: text;
+    animation: huerotate-ad62f0a1 5s linear infinite;
+}
 
+@keyframes huerotate-ad62f0a1 {
+    0% {
+    background-position: -500%;
+    }
+
+    100% {
+        background-position: 500%;
+    }
+}
 </style>
